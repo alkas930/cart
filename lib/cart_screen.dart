e118da4d51643b148743f7a8cd4e2e796573db8a
xpsAlka <alka.xportsoft@gmail.com>
2024-05-28 15:46:45 +0530

    update

---
+++ lib/cart_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CartScreen extends StatefulWidget {
+  final String productName;
+  final double price;
+  final String picture;
+  final String description;
+  int quantity;
+  CartScreen({
+    super.key,
+    required this.productName,
+    required this.price,
+    required this.picture,
+    required this.description,
+    required this.quantity,
+  });
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 245, 155, 207),
+        title: Text("Cart Screen"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: [
+          Container(
+            child: Center(
+              child: Image.asset(
+                widget.picture,
+                scale: 0.6,
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              widget.description,
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          Divider(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Row(
+              //   children: [
+              //     IconButton(
+              //       onPressed: () {
+              //         setState(() {
+              //           if (widget.quantity > 1) {
+              //             widget.quantity--;
+              //           }
+              //         });
+              //       },
+              //       icon: Icon(Icons.remove),
+              //     ),
+              //     Text(widget.quantity.toString()),
+              //     IconButton(
+              //       onPressed: () {
+              //         setState(() {
+              //           widget.quantity++;
+              //         });
+              //       },
+              //       icon: const Icon(Icons.add),
+              //     ),
+              //   ],
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "75 % off",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 5, 170, 60)),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "₹",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    widget.price.toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+
+              Row(
+                children: [
+                  Text(
+                    "Quantity ▶ ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                            color: const Color.fromARGB(255, 51, 50, 50))),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (widget.quantity > 1) {
+                                widget.quantity--;
+                              }
+                            });
+                          },
+                          icon: Icon(Icons.remove),
+                        ),
+                        Text(widget.quantity.toString()),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              widget.quantity++;
+                            });
+                          },
+                          icon: const Icon(Icons.add),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 30,
+                width: 100,
+                child: Center(
+                  child: Text(
+                    "Buy now",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              )),
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/product_screen.dart
@@ -0,0 +1,165 @@
+import 'package:add_to_cart/cart_screen.dart';
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatefulWidget {
+  ProductScreen({super.key});
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  final List<Map<String, dynamic>> products = [
+    {
+      "title": "Bag",
+      "price": 50.00,
+      "quantity": 1,
+      "image": "assets/bag.jpg",
+      "des":
+          "jir fashion Office Casual College Backpack School Bag For Men and Women Waterproof Backpack  (Brown, 30 L)"
+    },
+    {
+      "title": "Chair",
+      "price": 150.00,
+      "quantity": 1,
+      "image": "assets/chair.jpg",
+      "des":
+          "GEcreation Metal Living Room Chair  (Finish Color - Black, Pre-assembled)"
+    },
+    {
+      "title": "phone",
+      "price": 1550.00,
+      "quantity": 1,
+      "image": "assets/phone.jpg",
+      "des":
+          "OnePlus Nord 3 5G 128 GB 8 GB RAM Misty Green, Mobile Phone(493838380)"
+    },
+    {
+      "title": "shoes",
+      "price": 550.00,
+      "quantity": 1,
+      "image": "assets/shoes2.jpg",
+      "des": "FAST-FWD NITRO™ Elite Men's Running Shoes"
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 243, 184, 220),
+        title: const Text('Product Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (BuildContext context, index) {
+            return Column(
+              children: [
+                ListTile(
+                  leading: Image.asset(
+                    products[index]['image'],
+                    // scale: 2,
+                  ),
+                  title: Text(products[index]['title']),
+                  titleTextStyle: TextStyle(
+                      fontSize: 17,
+                      color: Color.fromARGB(115, 41, 40, 40),
+                      fontWeight: FontWeight.bold),
+                  subtitle: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          "${products[index]["des"]}  \n\n Price : ₹ ${products[index]['price'].toString()}"),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            width: 93,
+                            height: 38,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                    color:
+                                        const Color.fromARGB(255, 51, 50, 50))),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      if (products[index]['quantity'] > 1) {
+                                        products[index]['quantity']--;
+                                      }
+                                    });
+                                  },
+                                  icon: Icon(Icons.remove),
+                                ),
+                                Text(products[index]['quantity'].toString()),
+                                IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      products[index]['quantity']++;
+                                    });
+                                  },
+                                  icon: const Icon(Icons.add),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                height: 30,
+                                width: 55,
+                                child: Center(
+                                  child: Text(
+                                    "Buy now",
+                                    style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.amber,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                  trailing: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CartScreen(
+                            productName: products[index]['title'],
+                            price: products[index]['price'],
+                            picture: products[index]['image'],
+                            description: products[index]["des"],
+                            quantity: products[index]['quantity'],
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text('add to cart '),
+                  ),
+                ),
+                const Divider(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
